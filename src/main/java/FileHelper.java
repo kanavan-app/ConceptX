05e3691d39d1007c01ad73450ccb572325ef0b59
LAPTOP-HQ82GS0B\ckwong <kanavan.app@gmail.com>
2021-10-29 22:54:09 +0800

    add file helper class

---
+++ src/main/java/FileHelper.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public final class FileHelper {
+    private static FileHelper instance;
+    private final String separator;
+    private final String directory;
+    private final String folder = "data";
+    private final String extension = ".txt";
+
+    public static FileHelper getInstance() {
+        if (instance == null) {
+            instance = new FileHelper();
+        }
+        return instance;
+    }
+
+    private FileHelper() {
+        separator = System.getProperty("file.separator");
+        directory = System.getProperty("user.dir");
+    }
+
+    public void create(final String name) {
+        try {
+            final File file = new File(directory + separator + folder + separator + name + extension);
+            file.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String read(final String name) {
+        final StringBuilder builder = new StringBuilder();
+        try {
+            final File file = new File(directory + separator + folder + separator + name + extension);
+            final Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                final String data = scanner.nextLine();
+                builder.append(data).append("\n");
+            }
+            scanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    public void write(final String name, final String data) {
+        try {
+            final FileWriter fileWriter = new FileWriter(directory + separator + folder + separator + name + extension);
+            fileWriter.write(data);
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
